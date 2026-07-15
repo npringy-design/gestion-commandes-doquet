785dcb910ebaae2c79e9da08536c963c1ef740fd
npringy-design <npringy@gmail.com>
2026-07-15 08:17:21 +0200

    feat: mount order anomaly indicators

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import AppRouter from './components/AppRouter';
 import EnvironmentBanner from './components/EnvironmentBanner';
 import SyncStatusIndicator from './components/SyncStatusIndicator';
 import OrderFieldNavigationGuard from './components/OrderFieldNavigationGuard';
+import OrderAnomalyGuard from './components/OrderAnomalyGuard';
 
 const App: React.FC = () => {
   const state = useAppState();
@@ -30,6 +31,7 @@ const App: React.FC = () => {
     <>
       <EnvironmentBanner />
       <OrderFieldNavigationGuard />
+      <OrderAnomalyGuard state={state} />
       <AppRouter
         state={state}
         ratiosScrollRef={mainScrollRef}
